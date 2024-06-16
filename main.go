1316560aad934adad34fb9e9f1e68911b0d4a37d
Oscar R Acuña <147223484+oscarracuna@users.noreply.github.com>
2024-06-16 14:17:35 -0600

    Update main.go

--- main.go
+++ main.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"strings"
+	//"net"
 )
-
-var (
-	firstIP  string
-	secondIP string
-)
-
-type ips struct {
-	octet1 string
-	octet2 string
-	octet3 string
-	octet4 string
-}
 
 func main() {
+	var ipRange int
+
+	firstIP := "192.168.0.1"
 	fmt.Println("welcome to the basic ping scanner")
-	fmt.Println("Type 1st IP: ")
-	fmt.Scan(&firstIP)
-	fmt.Println("Type 2nd IP: ")
-	fmt.Scan(&secondIP)
-	log.Println(firstIP, secondIP)
+	fmt.Println("1st IP: ")
+	fmt.Scan()
+
+	firstIP_octets := strings.Split(firstIP, ".")
+	fmt.Println(firstIP_octets)
+	fmt.Println(firstIP_octets[3])
+
+	if firstIP_octets[3] != "0" {
+		ipRange = 0
+	}
+
+	for i := firstIP_octets[3]; i == "0"; {
+		fmt.Print("Pinging IP addresses...")
+	}
+	for j := ipRange; j <= 255; j++ {
+		fmt.Println("Pinging IP ending in: ", j)
+
+	}
 
 }
